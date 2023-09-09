925ea9babd83b560daaa8fe9aa2ab917ee7c0ab7
fabianmaurer <maurerfabi@gmail.com>
2023-09-09 23:42:46 +0200

    [TASK] Add sidebar + header

--- data.js
+++ data.js
@@ -1,3 +1,39 @@
 const dummyData = [
 
 ]
+
+const menus = [
+    {
+        selector: '#main-menu',
+        label: 'Management',
+        entries: [
+            {
+                icon: 'far fa-infinity',
+                label: 'Dashboard',
+                target: 'dashboard'
+            },
+            {
+                icon: 'far fa-list',
+                label: 'Printers list',
+                target: 'printer-list'
+            }
+        ]
+    },
+    {
+        selector: '#bottom-menu',
+        entries: [
+            {
+                icon: 'far fa-book-open',
+                label: 'Imprint'
+            },
+            {
+                icon: 'far fa-book-open',
+                label: 'Privacy'
+            },
+            {
+                icon: 'far fa-book-open',
+                label: 'Support'
+            }
+        ]
+    }
+]
--- logic.js
+++ logic.js
@@ -1,5 +1,36 @@
 function init() {
+    initMenus()
+}
 
+function initMenus() {
+    for(const menu of menus) {
+        const _menu = document.querySelector(menu.selector)
+        if (menu.label) {
+            const _label = document.createElement('span')
+            _label.classList.add('label')
+            _label.innerHTML = menu.label
+            _menu.appendChild(_label)
+        }
+        for(const entry of menu.entries) {
+            const _entry = document.createElement('li')
+            
+            const _entryInner = document.createElement('a')
+            _entry.innerHTML = '<i class="' + entry.icon + '"></i>'
+            _entryInner.addEventListener('click', () => {
+                transitionPage(entry.target)
+            })
+            _label = document.createElement('span')
+            _label.innerHTML = entry.label
+
+            _entryInner.appendChild(_label)
+            _entry.appendChild(_entryInner)
+            _menu.appendChild(_entry)
+        }
+    }
+}
+
+function transitionPage() {
+    
 }
 
 init()
